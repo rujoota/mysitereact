e0d86333ad3624b1d09befe5bb6c05e74d19bb16
rujoota.shah@gmail.com <rujoota.shah@gmail.com>
2019-03-11 21:42:43 -0700

    fixed security warnings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import About from './about'
-import Projects from './projects'
+import About from './components/about'
+import Projects from './components/projects'
 import './css/bootstrap.min.css'
 import './index.css';
 import './css/agency.min.css';
 import WebFont from 'webfontloader';
-import Blog from './blog'
-import Photos from './photos'
+import Blog from './components/blog'
+import Photos from './components/photos'
 import registerServiceWorker from './registerServiceWorker';
-import Contact from './contact';
+import Contact from './components/contact';
 WebFont.load({
     google: {
       families: ['Montserrat Web:400,700','Kaushan Script', 'Droid Serif:400,700,400italic,700italic', 'Roboto Slab:400,100,300,700']
@@ -26,9 +26,10 @@ function NavSubItems(props) {
 function NavigationBar(props) {
     return <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-custom" id="mainNav">
         <div className="container">  
-        <a className="navbar-brand" href="#page-top">Home<span className="sr-only">(current)</span></a>
+            <a className="navbar-brand" href="#page-top">Home<span className="sr-only">(current)</span></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
+            {/* <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i> */}
           </button>
           <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav ml-auto">
@@ -44,13 +45,13 @@ function MainHeader(props){
                 <div className="intro-text">
                     <div className="intro-lead-in">Hello World!</div>
                     <div className="intro-heading">It's Nice To Meet You</div>
-                    <a href="https://www.linkedin.com/in/rujoota" className="page-scroll btn btn-primary" target="_blank">
-                        <img src="./imgs/linkedin.png" width="25" /> My Linkedin
+                    <a href="https://www.linkedin.com/in/rujoota" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
+                        <img src="./imgs/linkedin.png" width="25" alt="Linkedin"/> My Linkedin
                         </a>
-                    <a href="https://github.com/rujoota" className="page-scroll btn btn-primary" target="_blank">
-                        <img src="./imgs/github.png" width="25" />My Github</a>
-                    <a href="http://rujoota.blogspot.in" className="page-scroll btn btn-primary" target="_blank">
-                        <img src="./imgs/blogspot.png" width="25" />My Blogs</a>
+                    <a href="https://github.com/rujoota" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
+                        <img src="./imgs/github.png" width="25" alt="Git"/>My Github</a>
+                    <a href="http://rujoota.blogspot.in" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
+                        <img src="./imgs/blogspot.png" width="25" alt="Blog"/>My Blogs</a>
                 </div>
             </div>
         </header>
@@ -72,10 +73,10 @@ function MainDOM(props){
         href:"contact",
         title:"contact"
     }]
-    return [<NavigationBar navitems={navItems}/>, <MainHeader />, <About />, <Projects />, <Blog />, <Photos />, <Contact />]
+    return [<NavigationBar navitems={navItems} key="NavigationBar"/>, <MainHeader key="MainHeader"/>, <About key="About"/>, <Projects key="Projects"/>, <Blog key="Blog"/>, <Photos key="Photos"/>, <Contact key="Contact"/>]
 }
 // <NavSubItems navitems={props.navitems} />
 
-const element = <MainDOM />;
+const element = <MainDOM key="MainDOM"/>;
 ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
