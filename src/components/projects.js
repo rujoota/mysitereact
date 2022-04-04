19a8bf2ee8ba3732c8428aa4bf54f97b32159978
Rujoota Shah <RujuRaj@Rujootas-MacBook-Pro-2.local>
2022-04-04 15:17:31 -0700

    removed unnessary images

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,86 +2,86 @@ import React, { Component } from 'react';
 import '../css/agency.min.css';
 import Parser from 'html-react-parser';
 class Projects extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { projs: [] }
-    }
-    componentDidMount() {
-        fetch('../data/projects.json')
-            .then(response => response.json())
-            .then(json => this.setState({ projs: json.projects }))
-    }
-    render() {
-        // console.log("state printing")
-        console.log(this.state.projs)
-        if(this.state.projs) {
-            return (
-                <section id="projects" className="bg-light-gray">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12 text-center">
-                                <h2 className="section-heading">My Projects</h2>
-                                <h3 className="section-subheading text-muted">Collection of my work</h3>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <ProjectBlock projects={this.state.projs} key='project-block'/>
-                        </div>
-                    </div>
-                </section>
-            );
-        }
-        
-    }
+	constructor(props) {
+		super(props)
+		this.state = { projs: [] }
+	}
+	componentDidMount() {
+		fetch('../data/projects.json')
+			.then(response => response.json())
+			.then(json => this.setState({ projs: json.projects }))
+	}
+	render() {
+		// console.log("state printing")
+		console.log(this.state.projs)
+		if (this.state.projs) {
+			return (
+				<section id="projects" className="bg-light-gray">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-12 text-center">
+								<h2 className="section-heading">My Projects</h2>
+								<h3 className="section-subheading text-muted">Collection of my work</h3>
+							</div>
+						</div>
+						<div className="row">
+							<ProjectBlock projects={this.state.projs} key='project-block' />
+						</div>
+					</div>
+				</section>
+			);
+		}
+
+	}
 }
 function ProjectDetails(props) {
-    return <div class="modal fade portfolio-modal" id={"portfolioModal" + props.project.order} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-        <div class="modal-dialog modal-full" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div className="modal-body">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div id={"project" + props.project.order + "-details"}>
-                                    {Parser(props.project.htmlDetails)}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-center">
-                        <button type="button" class="btn btn-primary project-center" data-dismiss="modal"><i
-                                class="fa fa-times"></i> Close Project
-                        </button>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-    </div>
+	return <div class="modal fade portfolio-modal" id={"portfolioModal" + props.project.order} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+		<div class="modal-dialog modal-full" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div className="modal-body">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12">
+								<div id={"project" + props.project.order + "-details"}>
+									{Parser(props.project.htmlDetails)}
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="project-center">
+						<button type="button" class="btn btn-primary project-center" data-dismiss="modal"><i
+							class="fa fa-times"></i> Close Project
+						</button>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
 }
 function ProjectBlock(props) {
-    return props.projects.map((proj) =>
-        <div style={{ float: 'none', margin: '0 auto' }}>
-            <div className="portfolio-item">
-                <a href="" className="portfolio-link" data-toggle="modal" data-target={"#portfolioModal" + proj.order} key={"project" + proj.order}>
-                    <div className="portfolio-hover">
-                        <div className="portfolio-hover-content">
-                            <i className="fa fa-hand-pointer-o fa-3x"></i>
-                        </div>
-                    </div>
-                    <img src={proj.img} className="img-fluid" alt="" />
-                </a>
-                <div className="portfolio-caption">
-                    <h4>{proj.name}</h4>
-                    <p className="text-muted">{proj.desc}</p>
-                </div>
-            </div>
-            <ProjectDetails project={proj}></ProjectDetails>
-        </div>)
+	return props.projects.map((proj) =>
+		<div style={{ float: 'none', margin: '0 auto' }}>
+			<div className="portfolio-item">
+				<a href="" className="portfolio-link" data-toggle="modal" data-target={"#portfolioModal" + proj.order} key={"project" + proj.order}>
+					<div className="portfolio-hover">
+						<div className="portfolio-hover-content">
+							<i className="fa fa-hand-pointer-o fa-3x"></i>
+						</div>
+					</div>
+					<img src={proj.img} className="img-fluid" alt="" />
+				</a>
+				<div className="portfolio-caption">
+					<h4>{proj.name}</h4>
+					<p className="text-muted">{proj.desc}</p>
+				</div>
+			</div>
+			<ProjectDetails project={proj}></ProjectDetails>
+		</div>)
 }
 export default Projects;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import About from './components/about'
 import Projects from './components/projects'
@@ -14,79 +13,79 @@ import Photos from './components/photos'
 // import registerServiceWorker from './registerServiceWorker';
 import Contact from './components/contact';
 WebFont.load({
-    google: {
-      families: ['Montserrat Web:400,700','Kaushan Script', 'Droid Serif:400,700,400italic,700italic', 'Roboto Slab:400,100,300,700']
-    }
-  });
+	google: {
+		families: ['Montserrat Web:400,700', 'Kaushan Script', 'Droid Serif:400,700,400italic,700italic', 'Roboto Slab:400,100,300,700']
+	}
+});
 function NavSubItems(props) {
-    let lis = props.navitems.map((navitem) =>
-        <li className="nav-item" key={navitem.title}>
-            <a className="nav-link" href={'#'+navitem.href} key={navitem.href}>{navitem.title}</a>
-        </li>
-    );
-    return (<ul key="mainul" className="navbar-nav ml-auto">{lis}</ul>)
+	let lis = props.navitems.map((navitem) =>
+		<li className="nav-item" key={navitem.title}>
+			<a className="nav-link" href={'#' + navitem.href} key={navitem.href}>{navitem.title}</a>
+		</li>
+	);
+	return (<ul key="mainul" className="navbar-nav ml-auto">{lis}</ul>)
 }
 function NavigationBar(props) {
-    return <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-custom" id="mainNav">
-        <div className="container">  
-            <a className="navbar-brand" href="#page-top">Home<span className="sr-only">(current)</span></a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            {/* <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i> */}
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav ml-auto">
-                <NavSubItems navitems={props.navitems} />
-            </ul>
-          </div>
-          </div>
-        </nav>
+	return <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-custom" id="mainNav">
+		<div className="container">
+			<a className="navbar-brand" href="#page-top">Home<span className="sr-only">(current)</span></a>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+				{/* <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i> */}
+			</button>
+			<div className="collapse navbar-collapse" id="navbarText">
+				<ul className="navbar-nav ml-auto">
+					<NavSubItems navitems={props.navitems} />
+				</ul>
+			</div>
+		</div>
+	</nav>
 }
-function MainHeader(props){
-   return <header id="mainheader">
-            <div className="container">
-                <div className="intro-text">
-                    <div className="intro-lead-in">Hello World!</div>
-                    <div className="intro-heading">It's Nice To Meet You</div>
-                    <a href="https://www.linkedin.com/in/rujoota" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
-                        <img src="./imgs/linkedin.png" width="25" alt="Linkedin"/> My Linkedin
-                        </a>
-                    <a href="https://github.com/rujoota" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
-                        <img src="./imgs/github.png" width="25" alt="Git"/>My Github</a>
-                    <a href="http://rujoota.blogspot.in" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
-                        <img src="./imgs/blogspot.png" width="25" alt="Blog"/>My Blogs</a>
-                </div>
-            </div>
-        </header>
+function MainHeader(props) {
+	return <header id="mainheader">
+		<div className="container">
+			<div className="intro-text">
+				<div className="intro-lead-in">Hello World!</div>
+				<div className="intro-heading">It's Nice To Meet You</div>
+				<a href="https://www.linkedin.com/in/rujoota" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
+					<img src="./imgs/linkedin.png" width="25" alt="Linkedin" /> My Linkedin
+				</a>
+				<a href="https://github.com/rujoota" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
+					<img src="./imgs/github.png" width="25" alt="Git" />My Github</a>
+				<a href="http://rujoota.blogspot.in" className="page-scroll btn btn-primary" target="_blank" rel="noopener noreferrer">
+					<img src="./imgs/blogspot.png" width="25" alt="Blog" />My Blogs</a>
+			</div>
+		</div>
+	</header>
 }
-function MainDOM(props){
-    const navItems = [{
-        href:"about",
-        title:"about"
-    }, {
-        href:"projects",
-        title:"projects"
-    }, {
-        href:"blog",
-        title:"blog"
-    }, {
-        href:"photolibrary",
-        title:"photo library"
-    }, {
-        href:"contact",
-        title:"contact"
-    }]
-    return [<NavigationBar navitems={navItems} key="NavigationBar"/>, <MainHeader key="MainHeader"/>, <About key="About"/>, <Projects key="Projects"/>, <Blog key="Blog"/>, <Photos key="Photos"/>, <Contact key="Contact"/>]
+function MainDOM(props) {
+	const navItems = [{
+		href: "about",
+		title: "about"
+	}, {
+		href: "projects",
+		title: "projects"
+	}, {
+		href: "blog",
+		title: "blog"
+	}, {
+		href: "photolibrary",
+		title: "photo library"
+	}, {
+		href: "contact",
+		title: "contact"
+	}]
+	return [<NavigationBar navitems={navItems} key="NavigationBar" />, <MainHeader key="MainHeader" />, <About key="About" />, <Projects key="Projects" />, <Blog key="Blog" />, <Photos key="Photos" />, <Contact key="Contact" />]
 }
 // <NavSubItems navitems={props.navitems} />
 
-const element = <MainDOM key="MainDOM"/>;
+const element = <MainDOM key="MainDOM" />;
 // ReactDOM.render(element, document.getElementById('root'));
 ReactDOM.render(
-  <React.StrictMode>
-    <MainDOM key="MainDOM"/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<MainDOM key="MainDOM" />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
